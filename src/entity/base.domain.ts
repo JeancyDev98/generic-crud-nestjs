118dc7d43c9190f75f3fead15b9b5c1903cbaf7b
Jean Enrique Palacios Estrada <jeanpalaciosdev@gmail.com>
2024-06-15 17:15:38 -0400

    add create & update types in domain

--- src/entity/base.domain.ts
+++ src/entity/base.domain.ts
@@ -1,5 +1,9 @@
 import { BasicPropsType } from './base.props';
 
+export type BasicCreateType<D> = Omit<D, keyof BasicDomainType<unknown, unknown>>
+
+export type BasicUpdateType<D> = Partial<D>;
+
 export type BasicDomainType<C, U> = BasicPropsType & {
   create(props: C, id?: string): BasicDomainType<C, U>;
   update(props: U, id?: string): BasicDomainType<C, U>;
